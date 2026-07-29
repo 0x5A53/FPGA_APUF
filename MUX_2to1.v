49fd3023383814b18c311d230d025a932e47c909
0x5A53 <zschwartz@protonmail.com>
2026-07-29 18:51:32 -0500

    Update MUX_2to1.v

--- MUX_2to1.v
+++ MUX_2to1.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/15/2026 06:56:02 PM
-// Design Name: 
-// Module Name: MUX_2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 (* dont_touch = "true" *)
 module MUX_2to1(
