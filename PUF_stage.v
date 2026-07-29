3b31d918b924d76fe2ab194ded9b832c89ebb003
0x5A53 <zschwartz@protonmail.com>
2026-07-29 18:52:00 -0500

    Clean up header comments in PUF_stage.v
    Removed header comments from PUF_stage.v

--- PUF_stage.v
+++ PUF_stage.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07/22/2026 06:54:42 PM
-// Design Name: 
-// Module Name: PUF_stage
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module PUF_stage
     #(
