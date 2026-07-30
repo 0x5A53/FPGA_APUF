ea1f48f66939d794641518346cff9259d0193180
0x5A53 <zschwartz@protonmail.com>
2026-07-29 19:01:24 -0500

    Implement full adder module in Verilog

--- Code/full_adder.v
+++ Code/full_adder.v
@@ -1,1 +1,12 @@
+`timescale 1ns / 1ps
 
+(* dont_touch = "true" *)
+module full_adder (
+    input a, b, cin,
+    output sum, cout
+    );
+    
+    assign sum = a ^ b ^ cin;
+    assign cout = (a&b) | (b&cin) | (a&cin);
+    
+endmodule
